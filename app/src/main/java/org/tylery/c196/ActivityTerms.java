fa0acde0e79afb1054ae1e678fa8f7ac941526d2
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-14 15:17:36 -0700

    add list of terms activity (init)

---
+++ app/src/main/java/org/tylery/c196/ActivityTerms.java
@@ -0,0 +1,14 @@
+package org.tylery.c196;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityTerms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms);
+    }
+}
