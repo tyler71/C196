468cc08145ac31e3115596b7ffc67f6ccd3c191d
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-16 16:18:09 -0700

    add room extras, add draw.io for uml

--- app/src/main/java/org/tylery/c196/ActivityTerms.java
+++ app/src/main/java/org/tylery/c196/ActivityTerms.java
@@ -3,12 +3,15 @@ package org.tylery.c196;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ActivityTerms extends AppCompatActivity {
+    private ListView listTerms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms);
+        listTerms = (ListView) findViewById(R.id.termListView);
     }
 }
