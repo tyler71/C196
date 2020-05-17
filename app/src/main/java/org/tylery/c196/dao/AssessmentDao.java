7d23cbc103b70c88577b61fd0baf8301c108a80c
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-17 13:22:03 -0700

    DAOs init and sync with drawio

---
+++ app/src/main/java/org/tylery/c196/dao/AssessmentDao.java
@@ -0,0 +1,7 @@
+package org.tylery.c196.dao;
+
+import androidx.room.Dao;
+
+@Dao
+public interface AssessmentDao {
+}
---
+++ app/src/main/java/org/tylery/c196/dao/NoteDao.java
@@ -0,0 +1,31 @@
+package org.tylery.c196.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import org.tylery.c196.entities.NoteEntity;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(NoteEntity note);
+
+    @Update
+    void update(NoteEntity note);
+
+    @Delete
+    void delete(NoteEntity note);
+
+    @Query("SELECT * FROM notes WHERE courseID= :courseID ORDER BY id ASC")
+    LiveData<List<NoteEntity>> getCourseNotes(int courseID);
+
+
+}
