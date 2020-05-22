7f19d3ed8b73d6b8fb7217cf41bb3eda5c7dbc95
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-21 17:32:25 -0700

    init each adapter

---
+++ app/src/main/java/org/tylery/c196/adapters/AssessmentAdapter.java
@@ -0,0 +1,6 @@
+package org.tylery.c196.adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AssessmentAdapter extends RecyclerView.Adapter {
+}
---
+++ app/src/main/java/org/tylery/c196/adapters/NoteAdapter.java
@@ -0,0 +1,6 @@
+package org.tylery.c196.adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteAdapter extends RecyclerView.Adapter {
+}
