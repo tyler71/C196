e88f08186e66837d664079874bfa0312e3468d6c
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-24 12:59:05 -0700

    create controller for edit course

--- app/src/main/java/org/tylery/c196/activities/ActivityEditCourse.java
+++ app/src/main/java/org/tylery/c196/activities/ActivityEditCourse.java
@@ -1,6 +1,16 @@
 package org.tylery.c196.activities;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.tylery.c196.R;
+
 public class ActivityEditCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_course);
+    }
 }
