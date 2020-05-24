be00b086c65f84f34b170989c0f4df6acbb21060
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-24 12:59:18 -0700

    etc

---
+++ app/src/main/java/org/tylery/c196/activities/CourseActivity.java
@@ -0,0 +1,16 @@
+package org.tylery.c196.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.tylery.c196.R;
+
+public class CourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
