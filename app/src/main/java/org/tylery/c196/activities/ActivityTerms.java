6a1c6321ab570eaa6f4f703686e023e0335243cc
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-26 10:16:30 -0700

    term adapter and activity.
    Can't interact but loads the terms in terms list

--- app/src/main/java/org/tylery/c196/activities/ActivityTerms.java
+++ app/src/main/java/org/tylery/c196/activities/ActivityTerms.java
@@ -1,19 +1,42 @@
 package org.tylery.c196.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.ListView;
 
 import org.tylery.c196.R;
+import org.tylery.c196.adapters.TermAdapter;
+import org.tylery.c196.entities.TermEntity;
+import org.tylery.c196.viewmodel.TermViewModel;
+
+import java.util.List;
 
 public class ActivityTerms extends AppCompatActivity {
-    private ListView listTerms;
+    private TermViewModel termViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_list);
-        listTerms = (ListView) findViewById(R.id.termListView);
+
+        RecyclerView recyclerView = findViewById(R.id.termListView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final TermAdapter adapter = new TermAdapter();
+        recyclerView.setAdapter(adapter);
+
+        termViewModel = new ViewModelProvider(this).get(TermViewModel.class);
+        termViewModel.getAllTerms().observe(this, new Observer<List<TermEntity>>() {
+            @Override
+            public void onChanged(List<TermEntity> termEntities) {
+                adapter.setNotes(termEntities);
+            }
+        });
+
     }
 }
--- app/src/main/java/org/tylery/c196/adapters/TermAdapter.java
+++ app/src/main/java/org/tylery/c196/adapters/TermAdapter.java
@@ -1,6 +1,58 @@
 package org.tylery.c196.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class TermAdapter extends RecyclerView.Adapter {
+import org.tylery.c196.R;
+import org.tylery.c196.entities.TermEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermHolder> {
+    private List<TermEntity> terms = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public TermHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.term_item, parent, false);
+        return new TermHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermHolder holder, int position) {
+        TermEntity currentTerm = terms.get(position);
+        holder.textViewTermTitle.setText(currentTerm.getTitle());
+        holder.textViewTermStartDate.setText(currentTerm.getStart());
+        holder.getTextViewTermEndDate.setText(currentTerm.getEnd());
+    }
+
+    @Override
+    public int getItemCount() {
+        return terms.size();
+    }
+
+    public void setNotes(List<TermEntity> terms) {
+        this.terms = terms;
+        notifyDataSetChanged();
+    }
+
+    class TermHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTermTitle;
+        private TextView textViewTermStartDate;
+        private TextView getTextViewTermEndDate;
+
+        public TermHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewTermTitle = itemView.findViewById(R.id.text_view_term_title);
+            textViewTermStartDate = itemView.findViewById(R.id.text_view_term_start_date);
+            getTextViewTermEndDate = itemView.findViewById(R.id.text_view_term_end_date);
+        }
+    }
 }
