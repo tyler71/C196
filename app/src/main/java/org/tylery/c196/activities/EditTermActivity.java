bac55a45bfa977528112290ead47fd787070f8a4
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-26 11:41:59 -0700

    add a term
    not functioning

--- app/src/main/java/org/tylery/c196/activities/EditTermActivity.java
+++ app/src/main/java/org/tylery/c196/activities/EditTermActivity.java
@@ -1,16 +1,80 @@
 package org.tylery.c196.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.tylery.c196.R;
 
 public class EditTermActivity extends AppCompatActivity {
+    public static final String EXTRA_TITLE =
+            "org.tylery.c196.activities.EXTRA_TITLE";
+    public static final String EXTRA_START_DATE =
+            "org.tylery.c196.activities.START_DATE";
+    public static final String EXTRA_END_DATE =
+            "org.tylery.c196.activities.END_DATE";
+
+    private EditText editTextTitle;
+    private EditText editTextStartDate;
+    private EditText editTextEndDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_term);
+
+        editTextTitle = findViewById(R.id.edit_term_title);
+        editTextStartDate = findViewById(R.id.edit_term_start_date);
+        editTextEndDate = findViewById(R.id.edit_term_end_date);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close_white_24dp);
+        setTitle("Editing Note");
+    }
+
+    private void saveTerm() {
+        String termTitle = editTextTitle.getText().toString();
+        String startDate = editTextStartDate.getText().toString();
+        String endDate   = editTextEndDate.getText().toString();
+
+        if(termTitle.trim().isEmpty()
+            || startDate.trim().isEmpty()
+            || endDate.trim().isEmpty()) {
+            Toast.makeText(this, "Empty fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_TITLE, termTitle);
+        data.putExtra(EXTRA_START_DATE, startDate);
+        data.putExtra(EXTRA_END_DATE, endDate);
+
+        setResult(RESULT_OK, data);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.edit_term, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.edit_term_save_button:
+                saveTerm();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/org/tylery/c196/activities/TermListActivity.java
+++ app/src/main/java/org/tylery/c196/activities/TermListActivity.java
@@ -1,12 +1,18 @@
 package org.tylery.c196.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.tylery.c196.R;
 import org.tylery.c196.adapters.TermAdapter;
@@ -16,12 +22,23 @@ import org.tylery.c196.viewmodel.TermViewModel;
 import java.util.List;
 
 public class TermListActivity extends AppCompatActivity {
+    public static final int ADD_TERM_REQUEST = 1;
+
     private TermViewModel termViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms_list);
+
+        FloatingActionButton buttonAddTerm = findViewById(R.id.btn_add_term);
+        buttonAddTerm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TermListActivity.this, EditTermActivity.class);
+                startActivityForResult(intent, ADD_TERM_REQUEST);
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.termListView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -38,5 +55,25 @@ public class TermListActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == ADD_TERM_REQUEST && resultCode == RESULT_OK) {
+
+//            TODO
+//              Likely convert these to UTC datetime
+            String title = data.getStringExtra(EditTermActivity.EXTRA_TITLE);
+            String startDate = data.getStringExtra(EditTermActivity.EXTRA_START_DATE);
+            String endDate = data.getStringExtra(EditTermActivity.EXTRA_END_DATE);
+
+            TermEntity termEntity = new TermEntity(title, startDate, endDate);
+            termViewModel.insert(termEntity);
+
+            Toast.makeText(this, "Term saved", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
