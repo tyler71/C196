466634cb7790ccdda874e974073f59f40a0f6f46
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-28 22:49:24 -0700

    add in assessment adapter for list of assessments

--- app/src/main/java/org/tylery/c196/adapters/AssessmentAdapter.java
+++ app/src/main/java/org/tylery/c196/adapters/AssessmentAdapter.java
@@ -1,6 +1,67 @@
 package org.tylery.c196.adapters;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-//public class AssessmentAdapter extends RecyclerView.Adapter {
-//}
+import org.tylery.c196.R;
+import org.tylery.c196.entities.AssessmentEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AssessmentAdapter extends RecyclerView.Adapter<AssessmentAdapter.AssessmentHolder> {
+   private List<AssessmentEntity> assessments = new ArrayList<>();
+   private OnItemClickListener listener;
+
+   @NonNull
+   @Override
+   public AssessmentHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View itemView = LayoutInflater.from(parent.getContext())
+               .inflate(R.layout.assessment_item, parent, false);
+       return new AssessmentHolder(itemView);
+   }
+
+   @Override
+   public void onBindViewHolder(@NonNull AssessmentHolder holder, int position) {
+       AssessmentEntity currentAssessment = assessments.get(position);
+       holder.textViewAssessmentTitle.setText(currentAssessment.getName());
+       holder.textViewAssessmentGoalDate.setText(currentAssessment.getGoalDate());
+   }
+
+   @Override
+   public int getItemCount() {
+      return assessments.size();
+   }
+
+   class AssessmentHolder extends RecyclerView.ViewHolder {
+      private TextView textViewAssessmentTitle;
+      private TextView textViewAssessmentGoalDate;
+
+      public AssessmentHolder(@NonNull View itemView) {
+         super(itemView);
+         textViewAssessmentTitle = itemView.findViewById(R.id.text_view_assessment_title);
+         textViewAssessmentGoalDate = itemView.findViewById(R.id.text_view_assessment_goal_date);
+
+         itemView.setOnClickListener(v -> {
+            int position = getAdapterPosition();
+            if(listener != null && position != RecyclerView.NO_POSITION) {
+               listener.onItemClick(assessments.get(position));
+            }
+         });
+      }
+   }
+
+   public interface OnItemClickListener {
+      void onItemClick(AssessmentEntity assessmentEntity);
+   }
+
+   public void setOnItemClickListener(OnItemClickListener listener) {
+      this.listener = listener;
+   }
+
+}
