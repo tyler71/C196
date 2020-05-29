1f772f30a8010b23ea35fb5f7ff4b7f692418eff
Tyler <4926565+tyler71@users.noreply.github.com>
2020-05-28 22:58:46 -0700

    continue for assessment

--- app/src/main/java/org/tylery/c196/activities/CourseAssessmentsListActivity.java
+++ app/src/main/java/org/tylery/c196/activities/CourseAssessmentsListActivity.java
@@ -1,16 +1,36 @@
 package org.tylery.c196.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import org.tylery.c196.R;
+import org.tylery.c196.viewmodel.AssessmentViewModel;
 
 public class CourseAssessmentsListActivity extends AppCompatActivity {
+    public static final int ADD_ASSESSMENT_REQUEST = 1;
+
+    public static final String EXTRA_COURSE_ID = "org.tylery.c196.activities.COURSE_ID";
+    public static final String EXTRA_COURSE_TITLE = "org.tylery.c196.activities.COURSE_TITLE";
+
+    private int courseID;
+    private String courseTitle;
+
+    private AssessmentViewModel assessmentViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_assessments_list);
+
+        Intent loadAssessmentsListIntent = getIntent();
+        courseID = loadAssessmentsListIntent.getIntExtra(EXTRA_COURSE_ID, -1);
+        courseTitle = loadAssessmentsListIntent.getStringExtra(EXTRA_COURSE_TITLE);
+
+        setTitle(courseTitle + " Assessments");
+
+//        RecyclerView recyclerView = findViewById(R.id.assessment)
     }
 }
