a99e1eed0ab5c32f738bbd3b7bb911fe60a48e40
Tyler <4926565+tyler71@users.noreply.github.com>
2020-06-01 14:17:46 -0700

    put in notification channel
    like notifications for alerts.. messages etc

--- app/src/main/java/org/tylery/c196/activities/NavigationPanelActivity.java
+++ app/src/main/java/org/tylery/c196/activities/NavigationPanelActivity.java
@@ -2,11 +2,16 @@ package org.tylery.c196.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
 
 import org.tylery.c196.R;
+
+import static org.tylery.c196.utils.AlarmReceiver.CHANNEL_ID_ALARMS;
 
 public class NavigationPanelActivity extends AppCompatActivity {
     private Button viewTermListBtn;
@@ -16,6 +21,9 @@ public class NavigationPanelActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_panel);
 
+//        Android 8+ requires notification channels
+        createNotificationChannels();
+
         viewTermListBtn = findViewById(R.id.btn_view_term_list);
         viewTermListBtn.setOnClickListener(v -> {
             Intent intent = new Intent(v.getContext(), TermListActivity.class);
@@ -23,4 +31,22 @@ public class NavigationPanelActivity extends AppCompatActivity {
         });
 
     }
+
+
+    private void createNotificationChannels() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Course Alarms";
+            String description = "When the course starts and ends";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID_ALARMS, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
